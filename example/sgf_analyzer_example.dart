144b8c6bac2aa9e7e5cfe7a05fd7f132ed4308a5
cleong98 <zhuzhuloh1998@gmail.com>
2023-07-05 03:25:32 +0800

    first commit

---
+++ example/sgf_analyzer_example.dart
@@ -0,0 +1,6 @@
+import 'package:sgf_analyzer/sgf_analyzer.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/sgf_analyzer.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/sgf_analyzer_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
